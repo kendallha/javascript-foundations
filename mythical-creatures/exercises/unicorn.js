a17ab3e05f09a08d5b0686ab95a59ea4bc81ab1e
kendallha <kendallhaw@gmail.com>
2021-02-08 19:07:42 -0800

    Add unicorn functions and remove unicorn test skips

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,27 @@
 class Unicorn {
   constructor(name, color) {
     this.name = name;
+    if (!color) {
+      this.color = "white";
+    } else {
     this.color = color;
+    }
   }
-}
+    isWhite() {
+      if (this.color !== "white") {
+        return false;
+      }
+    }
+    says(message) {
+      return `**;* ${message} *;**`;
+    }
+  }
+
+
 
 module.exports = Unicorn;
+
+//read the test
+//ask yourself if it will fail and why
+//run the test
+//make it pass
