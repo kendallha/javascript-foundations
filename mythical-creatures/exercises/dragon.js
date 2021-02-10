3e7dedc54cc36faa3e0d59aeb458837ffc51030b
kendallha <kendallhaw@gmail.com>
2021-02-10 07:35:13 -0800

    Complete dragon exercise and remove test skips

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+var timesEaten = 0;
+class Dragon {
+  constructor(name, rider, hungry, timesEaten) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
 
+  }
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    timesEaten++;
+    if (timesEaten >= 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+
+module.exports = Dragon;
